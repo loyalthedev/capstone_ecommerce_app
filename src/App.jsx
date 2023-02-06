98d25509e13c5baf54501b2575a93e72e76dc7ee
daddyloyal <lifeofemmany6@gmail.com>
2023-02-06 14:14:35 +0100

    Latest changes made

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import CategoryItem from "./components/category-item/CategoryItem";
 import './categories.styles.scss';
 function App() {
   const categories = [
@@ -31,16 +32,8 @@ function App() {
 
   return (
     <div className="App categories-container">
-      {categories.map(({title, id, imageUrl}) => (
-        <div className="category-container" key={id}>
-        <div className="background-image" style={{
-          backgroundImage: `url(${imageUrl})`
-        }}/>
-        <div className="category-body-container">
-          <h2 className="text-2xl font-semibold">{title}</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
       ))}
     </div>
   );
