eeb6f31a782c9e5068be2f3390391e4d45489ff0
daddyloyal <lifeofemmany6@gmail.com>
2023-02-10 00:19:19 +0100

    Added our sign in component as well as firebase utils and brought in firebase libraries

--- src/utils/firebase/Firebase.js
+++ src/utils/firebase/Firebase.js
@@ -6,6 +6,13 @@ import { getAuth,
          GoogleAuthProvider 
         } from 'firebase/auth';
 
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc
+} from 'firebase/firestore'
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCF-nrfgM8Y_4icOJ3R7XNFH9LkZ36k7AU",
@@ -27,3 +34,38 @@ provider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+// DataBase Initialization
+export const db = getFirestore()
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, 'users', userAuth.uid);
+
+  console.log(userDocRef);
+
+  const userSnapShot = await getDoc(userDocRef);
+
+  console.log(userSnapShot);
+  console.log(userSnapShot.exists());
+
+  if(userSnapShot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt
+      });
+    } catch (error) {
+      console.log("Error creating the user", error.message);
+    }
+  }
+  
+  return userDocRef;
+
+  // If user data doest not exist
+  // Create / set the document with the data from the userAuth in my collection
+  // Return userDocRef
+
+}
